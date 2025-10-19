e35ac45edffcd730bab11621937c3781b850cd94
Mohammad Sadeq Kazemi <mskdev.ir@gmail.com>
2025-10-19 16:38:25 +0330

    add popular product section and handle add to cart function on local storage

--- src/components/Pages/index.jsx
+++ src/components/Pages/index.jsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Autoplay, EffectFade } from "swiper/modules";
 import "swiper/css/effect-fade";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 import heroImg from "../../assets/images/hero.png";
 import heroImg2 from "../../assets/images/hero-2.png";
@@ -33,6 +34,26 @@ import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Index = () => {
+  const products = ProductData.Products;
+  const specialOffer = products.find((p) => p.Id === 7);
+
+  const navigate = useNavigate();
+
+  const handleAddToCart = (product) => {
+    const cart = JSON.parse(localStorage.getItem("cartItems")) || [];
+    const exists = cart.some((item) => item.Id === product.Id);
+
+    if (!exists) {
+      const updateCart = [...cart, { ...product, quantity: 1 }];
+      localStorage.setItem("cartItems", JSON.stringify(updateCart));
+      toast.success("Item Added To Cart");
+    } else {
+      toast.warning("Item Already In Cart");
+    }
+    setTimeout(() => {
+      navigate("/cart");
+    }, 1000);
+  };
   return (
     <>
       <div className="bg-element"></div>
@@ -198,6 +219,90 @@ const Index = () => {
             </h3>
             <p className="text-base md:text-lg">Today's Super Offer</p>
           </div>
+        </div>
+      </div>
+      {/* Section Title */}
+      <div className="container mx-auto section-title my-10">
+        <span className="text-xl font-semibold bg-yellow-300 px-5 py-2 rounded-full">
+          Our Products
+        </span>
+        <h1 className="text-5xl font-bold font-bricolage mt-5">
+          Popular Products
+        </h1>
+      </div>
+      {/* Products */}
+      <div className="container mx-auto product-wrapper py-10 grid grid-cols-1 lg:grid-cols-4 gap-4">
+        {/* Specian Offer Card */}
+        <div className="bg-white border-2 border-yellow-400 p-6 product-banner-wrap rounded-xl flex flex-col items-center justify-center text-center relative">
+          <span className="text-xl text-white font-bold mb-1 bg-red-600 px-3 py-2 rounded">
+            Special Offer
+          </span>
+          <div className="absolute top-4 right-4 bg-yellow-400 text-white rounded-full h-16 w-16 flex items-center justify-center font-bold text-sm">
+            Save <br /> $120
+          </div>
+          <img
+            src={specialOffer.ProductsImage}
+            alt={specialOffer.Name}
+            className="w-4/5 mx-auto my-4"
+          />
+          <h3 className="text-yellow-800 font-semibold">{specialOffer.Name}</h3>
+          <div className="mt-2">
+            <span className="line-through text-gray-500">
+              ${specialOffer.OldPrice}
+            </span>
+            <span className="text-red-600 text-xl font-bold">
+              ${specialOffer.Price}
+            </span>
+          </div>
+          <div className="flex justify-between w-full mt-4 text-sm">
+            <span>Available: 6</span>
+            <span>Already Sold: 27</span>
+          </div>
+          <div className="w-full bg-gray-200 h-2 rounded-full mt-1 overflow-hidden">
+            <div className="bg-yellow-400 w-1/5 h-full"></div>
+          </div>
+          <p className="mt-3 text-gray-700 text-sm">Hurry Up! Offer Ends In:</p>
+        </div>
+        {/* Products Cards */}
+        <div className="lg:col-span-3">
+          <div className="grid product-wrap grid-cols-2 md:grid-cols-4 gap-6">
+            {products.slice(0, 8).map((product) => (
+              <div
+                key={product.Id}
+                className="bg-white shadow-md rounded-xl p-4 flex flex-col items-start hover:shadow-xl transition duration-300 group border border-gray-100 cursor-pointer"
+              >
+                <p className="text-xs text-white font-bold mb-1 bg-red-600 px-3 py-2 rounded">
+                  {product.Category}
+                </p>
+                <img
+                  src={product.ProductsImage}
+                  alt={product.Name}
+                  className="w-4/5 h-32 object-contain group-hover:scale-105 transition-transform duration-300"
+                  onClick={() => navigate(`/product/${product.Id}`)}
+                />
+                <h4
+                  onClick={() => navigate(`/product/${product.Id}`)}
+                  className="text-lg font-medium mt-3 text-yellow-800 hover:underline line-clamp-2"
+                >
+                  {product.Name}
+                </h4>
+                <div className="flex mt-5 flex-row items-center justify-between w-full">
+                  <div className="mt-1 text-md">
+                    <span className="text-lg font-semibold mt-1">
+                      ${product.Price}
+                    </span>
+                  </div>
+                  <button
+                    className="bg-yellow-400 text-white rounded-full w-[45px] h-[45px] hover:bg-red-500 hover:shadow-xl transition"
+                    onClick={() => handleAddToCart(product)}
+                  >
+                    <AiOutlineShoppingCart className="m-auto" />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <ToastContainer position="top-right" autoClose={1500} />
         </div>
       </div>
     </>
