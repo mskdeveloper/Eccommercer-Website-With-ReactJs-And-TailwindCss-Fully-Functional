95cb3a0d40df25e9c8e9b936987b2331f5f4e68e
Mohammad Sadeq Kazemi <mskdev.ir@gmail.com>
2025-10-19 13:01:20 +0330

    add banner section

--- src/components/Pages/index.jsx
+++ src/components/Pages/index.jsx
@@ -131,6 +131,75 @@ const Index = () => {
           </Swiper>
         </header>
       </div>
+      {/* Banners */}
+      <div className="px-[8%] lg:px-[12%] py-20">
+        <div
+          className="container mx-auto banner-1 flex flex-col justify-center gap-5 bg-cover bg-center rounded-xl p-6 md:p-8 h-[350px] sm:h-[430px]"
+          style={{ backgroundImage: `url(${banner5})` }}
+        >
+          <small className="bg-yellow-500 text-white text-xl px-4 py-2 w-fit rounded-md rounded-tl-none">
+            EXCLUSIVE HEADPHONE
+          </small>
+          <h3 className="text-7xl font-semibold font-bricolage">
+            Release Date & Price
+          </h3>
+          <p className="text-2xl">Today's Super Offer</p>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 mt-12">
+          {/* Baner1 */}
+          <div
+            className="flex flex-col gap-2 bg-cover bg-center rounded-xl p-6 md:p-8 h-[350px] sm:h-[430px]"
+            style={{ backgroundImage: `url(${banner1})` }}
+          >
+            <small className="bg-yellow-500 text-white text-xl px-4 py-2 w-fit rounded-md rounded-tl-none">
+              New Product
+            </small>
+            <h3 className="text-xl md:text-2xl font-semibold font-bricolage">
+              Release Date & Price
+            </h3>
+            <p className="text-base md:text-lg">Today's Super Offer</p>
+          </div>
+          {/* Baner2 */}
+          <div
+            className="flex flex-col gap-2 bg-cover bg-center rounded-xl p-6 md:p-8 h-[350px] sm:h-[430px]"
+            style={{ backgroundImage: `url(${banner2})` }}
+          >
+            <small className="bg-yellow-500 text-white text-xl px-4 py-2 w-fit rounded-md rounded-tl-none">
+              New Product
+            </small>
+            <h3 className="text-xl md:text-2xl font-semibold font-bricolage">
+              Release Date & Price
+            </h3>
+            <p className="text-base md:text-lg">Today's Super Offer</p>
+          </div>
+          {/* Baner3 */}
+          <div
+            className="flex flex-col gap-2 bg-cover bg-center rounded-xl p-6 md:p-8 h-[350px] sm:h-[430px]"
+            style={{ backgroundImage: `url(${banner3})` }}
+          >
+            <small className="bg-yellow-500 text-white text-xl px-4 py-2 w-fit rounded-md rounded-tl-none">
+              New Product
+            </small>
+            <h3 className="text-xl md:text-2xl font-semibold font-bricolage">
+              Release Date & Price
+            </h3>
+            <p className="text-base md:text-lg">Today's Super Offer</p>
+          </div>
+          {/* Baner4 */}
+          <div
+            className="flex flex-col gap-2 bg-cover bg-center rounded-xl p-6 md:p-8 h-[350px] sm:h-[430px]"
+            style={{ backgroundImage: `url(${banner4})` }}
+          >
+            <small className="bg-yellow-500 text-white text-xl px-4 py-2 w-fit rounded-md rounded-tl-none">
+              New Product
+            </small>
+            <h3 className="text-xl md:text-2xl font-semibold font-bricolage">
+              Release Date & Price
+            </h3>
+            <p className="text-base md:text-lg">Today's Super Offer</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
