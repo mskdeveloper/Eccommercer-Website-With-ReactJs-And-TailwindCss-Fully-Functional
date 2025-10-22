7b89755809287e4e0120b64ea8c98efd949b2f49
Mohammad Sadeq Kazemi <mskdev.ir@gmail.com>
2025-10-22 18:22:32 +0330

    Add another section in product details page like policy, add review and brands

--- src/components/Pages/Cart.jsx
+++ src/components/Pages/Cart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Cart = () => {
   const products = localStorage.getItem("cartItems");
-  console.log(products);
+  // console.log(products);
   return <div></div>;
 };
 
